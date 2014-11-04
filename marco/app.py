30ea891d9d59f71a239c2c41e0f604d3026a0b57
tonic <tonicbupt@gmail.com>
2014-11-04 16:59:02 +0800

    clean code

--- marco/app.py
+++ marco/app.py
@@ -31,13 +31,9 @@ def load_config():
 
 def create_app():
     config = load_config()
-    #binds = {
-    #    'marco': 'mysql://{username}:{password}@{host}:{port}/{db}'.format(**config['mysql'])
-    #}
 
     app = Flask(__name__, static_url_path='/marco/static')
     app.config.update(config)
-    #app.config.update(SQLALCHEMY_BINDS=binds)
 
     db.init_app(app)
     etcd.init_app(app)
@@ -49,9 +45,5 @@ def create_app():
     for bp in blueprints:
         import_name = '%s.views.%s:bp' % (__package__, bp)
         app.register_blueprint(import_string(import_name))
-
-    #with app.app_context():
-    #    db.drop_all(bind='marco')
-    #    db.create_all(bind='marco')
 
     return app
