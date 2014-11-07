c7f6375117441ab7476620a42849bd9144bb99fc
Neuron Teckid <Lene13@gmail.com>
2014-11-07 11:49:55 +0800

    fix graph display and mouse hover; fix duplicate data in one timestamp

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 # coding: utf-8
 
+import sys
+
 from marco.app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000, debug=True)
+    port = 5000
+    if len(sys.argv) == 2:
+        port = int(sys.argv[1])
+    app.run('0.0.0.0', port, debug=True)
