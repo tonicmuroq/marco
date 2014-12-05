1f6c79cfb5267444c91f8a324e347cc0c9926a9b
tonic <tonicbupt@gmail.com>
2014-12-05 16:40:42 +0800

    dot grouped

--- marco/ext/dot_ext.py
+++ marco/ext/dot_ext.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from urlparse import urlparse
-from flask import current_app, _app_ctx_stack
+from flask import current_app, g, _app_ctx_stack
 
 from marco.dot import DotClient
 
@@ -15,18 +15,23 @@ class Dot(object):
     def init_app(self, app):
         app.config.setdefault('DOT_URL', 'http://localhost:5000')
 
-    def connect(self):
-        dot_uri = current_app.config['DOT_URL']
+    def connect(self, env):
+        dot_uri = current_app.config['DOT_URL_%s' % env.upper()]
         parsed = urlparse(dot_uri)
         return DotClient(host=parsed.hostname, port=parsed.port, scheme=parsed.scheme)
 
-    @property
-    def dot(self):
+    def dot(self, env):
         ctx = _app_ctx_stack.top
         if ctx is not None:
-            if not hasattr(ctx, 'dot'):
-                ctx.dot = self.connect()
-            return ctx.dot
+            if env == 'online':
+                if not hasattr(ctx, 'dot_online'):
+                    ctx.dot_online = self.connect(env)
+                return ctx.dot_online
+            if env == 'intra':
+                if not hasattr(ctx, 'dot_intra'):
+                    ctx.dot_intra = self.connect(env)
+                return ctx.dot_intra
 
     def __getattr__(self, name):
-        return getattr(self.dot, name)
+        dot = self.dot(g.dot_env)
+        return getattr(dot, name)
